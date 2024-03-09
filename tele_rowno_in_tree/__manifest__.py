1599d802934be51f9f43fc3073ad4fac5e8f6303
TeleNoc <40357190+telenoc@users.noreply.github.com>
2024-03-09 16:37:18 +0500

    Add files via upload

---
+++ tele_rowno_in_tree/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    "name": "Tele Row Number in tree/list view",
+    "version": "17.0.1.0.0",
+    "summary": "Show row number in tree/list view.",
+    "category": "Other",
+    "depends": ["web"],
+    "data": [],
+    "assets": {
+        "web.assets_backend": [
+            "tele_rowno_in_tree/static/src/views/list/list_render.xml",
+            "tele_rowno_in_tree/static/src/js/list_renderer.esm.js",
+        ]
+    },
+    "images": ["static/description/banner.png"],
+    "license": "LGPL-3",
+    "installable": True,
+    "application": True,
+    "auto_install": False,
+}
