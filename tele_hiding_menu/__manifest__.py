512f3506ffdcc4f029c321a3b5e9ab3e611e81a0
TeleNoc <40357190+telenoc@users.noreply.github.com>
2024-03-09 15:18:47 +0500

    Add files via upload

---
+++ tele_hiding_menu/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': "Tele Hiding Menu",
+    'summary': """
+        Restrict Menu Items from Specific Users""",
+    'description': """
+        Restrict Menu Items from Specific Users""",
+    'author': 'TeleNoc',
+    'maintainer':'TeleNoc',
+    'website': "https://www.telenoc.org/",
+    'images': ["static/description/banner.jpg"],
+    'category': 'Extra Rights',
+    'version': "17.0",
+    'license': 'AGPL-3',
+    'depends': [
+        'base'
+    ],
+    'data': ['views/res_users.xml',
+             ],
+}
