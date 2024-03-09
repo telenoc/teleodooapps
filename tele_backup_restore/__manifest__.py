d61d9ddb70273882a3d65748c56baf3e2296e18e
TeleNoc <40357190+telenoc@users.noreply.github.com>
2024-03-09 15:39:56 +0500

    Add files via upload

---
+++ tele_backup_restore/__manifest__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+{
+  "name"                 :  "Tele Odoo Database Backup",
+  "summary"              :  """Module provide feature to admin to take backups of his instance's database and later download them.""",  
+  "category"             :  "Extra Tools",
+  "version"              :  "1.0.1",
+  "author"               :  "TeleNoc",
+  "license"              :  "Other proprietary",
+  "website"              :  "https://telenoc.org/",
+  "description"          :  """Module provide feature to admin to take backups of his instance's database and later download them.""",
+  "live_test_url"        :  "http://",
+  "depends"              :  [
+                             'base',
+                            ],
+  "data"                 :  [
+                             'security/ir.model.access.csv',
+                             'wizards/backup_deletion_confirmation_view.xml',
+                             'data/backup_process_sequence.xml',
+                             'views/backup_process.xml',
+                             'data/backup_ignite_crone.xml',
+                             'views/menuitems.xml',
+                            ],
+  "images"               :  ['static/description/Banner.jpg'],
+  "application"          :  True,
+  "installable"          :  True,
+  "currency"             :  "USD",
+  "pre_init_hook"        :  "pre_init_check",
+  "external_dependencies":  {'python': ['python-crontab', 'cssselect']},
+}
