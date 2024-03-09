f50ef7e4ca64d4625769054a4c671e6bd3693283
TeleNoc <40357190+telenoc@users.noreply.github.com>
2024-03-09 20:44:51 +0500

    Add files via upload

---
+++ tele_database_cleanup/tests/__init__.py
@@ -0,0 +1,11 @@
+
+from . import common
+from . import test_create_indexes
+from . import test_identifier_adapter
+from . import test_purge_columns
+from . import test_purge_data
+from . import test_purge_menus
+from . import test_purge_models
+from . import test_purge_modules
+from . import test_purge_properties
+from . import test_purge_tables
