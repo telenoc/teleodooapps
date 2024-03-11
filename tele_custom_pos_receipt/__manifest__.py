40b72c2753cc89abd5b5ac8826d35d990ad8b36b
TeleNoc <40357190+telenoc@users.noreply.github.com>
2024-03-11 20:28:07 +0500

    Add files via upload

---
+++ tele_custom_pos_receipt/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+
+{
+    'name': 'Tele POS Custom Receipt',
+    'version': '1.0',
+    "license": "OPL-1",
+    'depends': ['base', 'point_of_sale'],
+    'category': 'Sales/Point of Sale',
+    'summary': 'This module is used to customized receipt of point of sale when a user adds a product in the cart and validates payment and print receipt, then the user can see the client name on POS Receipt. | Custom Receipt | POS Reciept | Payment | POS Custom Receipt',
+    'description': "Customized our point of sale receipt.",
+    'assets': {
+        'point_of_sale._assets_pos': [
+            'tele_custom_pos_receipt/static/src/**/*',
+        ],
+    },
+    'images': ['static/description/banner.jpeg'],
+    'installable': True,
+}
