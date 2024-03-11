7e5d6be34b0a200a7358b49b58e7d96216f7f6a9
TeleNoc <40357190+telenoc@users.noreply.github.com>
2024-03-11 13:01:31 +0500

    Add files via upload

---
+++ tele_queue_job/wizards/queue_jobs_to_done.py
@@ -0,0 +1,13 @@
+
+from odoo import models
+
+
+class SetJobsToDone(models.TransientModel):
+    _inherit = "queue.requeue.job"
+    _name = "queue.jobs.to.done"
+    _description = "Set all selected jobs to done"
+
+    def set_done(self):
+        jobs = self.job_ids
+        jobs.button_done()
+        return {"type": "ir.actions.act_window_close"}
