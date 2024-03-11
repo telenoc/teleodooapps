990d907cef4a26a08398c3a780f9cbf3d43cdb58
TeleNoc <40357190+telenoc@users.noreply.github.com>
2024-03-11 18:20:58 +0500

    Add files via upload

---
+++ tele_export_stockinfo_excel/wizard/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+###############################################################################
+#
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+###############################################################################
+from . import stock_report
