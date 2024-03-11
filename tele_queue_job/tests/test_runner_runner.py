5266ef3e06cf053e54bf3ec3205913a720dbad0a
TeleNoc <40357190+telenoc@users.noreply.github.com>
2024-03-11 13:00:24 +0500

    Add files via upload

---
+++ tele_queue_job/tests/test_runner_runner.py
@@ -0,0 +1,7 @@
+# pylint: disable=odoo-addons-relative-import
+# we are testing, we want to test as we were an external consumer of the API
+from odoo.addons.queue_job.jobrunner import runner
+
+from .common import load_doctests
+
+load_tests = load_doctests(runner)
