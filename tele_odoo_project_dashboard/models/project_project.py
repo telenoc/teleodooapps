5d4b9858fbd273804e2dc6b9dd9ccd95d7c68864
TeleNoc <40357190+telenoc@users.noreply.github.com>
2024-03-11 18:26:37 +0500

    Add files via upload

---
+++ tele_odoo_project_dashboard/models/project_project.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+import random
+from odoo import models
+
+
+class ProjectProject(models.Model):
+    """This class inherits from 'project.project' and adds custom functionality
+    to it.It provides methods to work with project data."""
+    _inherit = 'project.project'
+
+    def get_color_code(self):
+        """Generate a random color code in hexadecimal format.
+        :return: A random color code in the format '#RRGGBB.'"""
+        color = f"#{random.randint(0, 0xFFFFFF):06x}"
+        return color
