bfc25a6313d0682a5c9981196491cfccb218b67c
TeleNoc <40357190+telenoc@users.noreply.github.com>
2024-03-12 17:14:34 +0500

    Add files via upload

---
+++ tele_product_label/wizard/print_product_label_line.py
@@ -0,0 +1,45 @@
+
+from odoo import api, fields, models
+
+
+class PrintProductLabelLine(models.TransientModel):
+    _name = "print.product.label.line"
+    _description = 'Line with a Product Label Data'
+    _order = 'sequence'
+
+    sequence = fields.Integer(default=900)
+    selected = fields.Boolean(string='Print', default=True)
+    wizard_id = fields.Many2one(comodel_name='print.product.label')  # Not make required
+    product_id = fields.Many2one(comodel_name='product.product', required=True)
+    barcode = fields.Char(compute='_compute_barcode')
+    qty_initial = fields.Integer(string='Initial Qty', default=1)
+    qty = fields.Integer(string='Label Qty', default=1)
+    company_id = fields.Many2one(
+        comodel_name='res.company',
+        compute='_compute_company_id',
+    )
+
+    @api.depends('wizard_id.company_id')
+    def _compute_company_id(self):
+        for label in self:
+            label.company_id = \
+                label.wizard_id.company_id and label.wizard_id.company_id.id \
+                or self.env.user.company_id.id
+
+    @api.depends('product_id')
+    def _compute_barcode(self):
+        for label in self:
+            label.barcode = label.product_id.barcode
+
+    def action_plus_qty(self):
+        self.ensure_one()
+        if not self.qty:
+            self.update({'selected': True})
+        self.update({'qty': self.qty + 1})
+
+    def action_minus_qty(self):
+        self.ensure_one()
+        if self.qty > 0:
+            self.update({'qty': self.qty - 1})
+            if not self.qty:
+                self.update({'selected': False})
