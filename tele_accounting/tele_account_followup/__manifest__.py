20a07d6f921a1fcc5aacffe8e0a04d7e74f064db
TeleNoc <40357190+telenoc@users.noreply.github.com>
2024-03-12 21:11:09 +0500

    Add files via upload

---
+++ tele_accounting/tele_account_followup/__manifest__.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Tele Customer Follow Up Management',
+    'version': '17.0.1.0.0',
+    'category': 'Accounting',
+    'description': """Customer FollowUp Management""",
+    'summary': """Customer FollowUp Management""",
+    'author': 'TeleNoc',
+    'license': 'LGPL-3',
+    'website': 'https://www.telenoc.org',
+    'depends': ['account', 'mail'],
+    'data': [
+        'security/security.xml',
+        'security/ir.model.access.csv',
+        'data/data.xml',
+        'wizard/followup_print_view.xml',
+        'wizard/followup_results_view.xml',
+        'views/followup_view.xml',
+        'views/account_move.xml',
+        'views/partners.xml',
+        'views/report_followup.xml',
+        'views/reports.xml',
+        'views/followup_partner_view.xml',
+        'report/followup_report.xml',
+    ],
+    'demo': ['demo/demo.xml'],
+    'images': ['static/description/banner.jpeg'],
+    'installable': True,
+    'auto_install': False,
+}
