a81f9dbd7e72e00c84e001c43d3dbf6bdf142de3
TeleNoc <40357190+telenoc@users.noreply.github.com>
2024-03-12 17:33:39 +0500

    Add files via upload

---
+++ tele_product_min_price/models/product.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+
+class ProductTemplate(models.Model):
+    _inherit = 'product.template'
+    
+    purchase_min_price = fields.Float(string='Purchase price', compute='_get_min_purchase_price', digits='Product Price')
+
+    @api.depends('seller_ids')
+    def _get_min_purchase_price(self):
+        for rec in self:
+            seller_price = rec.seller_ids.filtered(lambda s: (not s.date_start or s.date_start <= fields.Date.today()) and
+                                                              (not s.date_end or s.date_end >= fields.Date.today())).mapped('price')
+            rec.purchase_min_price = seller_price and min(seller_price) or 0
+
+class ProductProduct(models.Model):
+    _inherit = 'product.product'
+
+    purchase_min_price = fields.Float(string='Purchase price', compute='_get_min_purchase_price', digits='Product Price')
+
+    @api.depends('seller_ids')
+    def _get_min_purchase_price(self):
+        for rec in self:
+            seller_price = rec.variant_seller_ids.filtered(lambda s: (not s.product_id or s.product_id == rec) and
+                                                                      (not s.date_start or s.date_start < fields.Date.today()) and
+                                                                      (not s.date_end or s.date_end > fields.Date.today())).mapped('price')
+            rec.purchase_min_price = seller_price and min(seller_price) or 0
