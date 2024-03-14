1ef2cab01f9773c4455bf382e7d35115e1f016e7
TeleNoc <40357190+telenoc@users.noreply.github.com>
2024-03-14 15:23:47 +0500

    Add files via upload

---
+++ tele_birthday_reminder/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Tele Birthday Wish to Customer and Employee",
+    "version": "17.0",
+    "category": "HR",
+    "summary": "This Module send an email to customer and employee",
+    "description": """
+    This module send an email to customer and employee. also this one 
+    will send reminder to all other employee and manager.
+    """,
+    "author": "TeleNoc",
+    "website": "http://telenoc.org",
+    "support": "contact@telenoc.org",
+    "depends": ['contacts', 'hr'],
+    "data": [
+        "data/ir_cron.xml",
+        "data/mail_template_demo.xml"
+    ],
+    "images": ["images/banner.jpeg"],
+    "license": "OPL-1",
+    "installable": True,
+    'license': 'LGPL-3',
+    "price": 00,
+    "currency": "USD",
+}
